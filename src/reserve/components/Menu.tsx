a1bee9ef6f4c6cbd2b7da869a709d7a1b541c8b7
Y.Hisamatsu <bonta0902@gmail.com>
2023-01-06 01:29:19 +0000

    modify

--- src/reserve/components/Menu.tsx
+++ src/reserve/components/Menu.tsx
@@ -3,17 +3,18 @@ import { forwardRef } from 'react'
 
 export interface MenutProps {
 	currCalendars: ReserveMatrixData | null
+	disabled?: boolean
 }
 
 export const Menu = forwardRef<HTMLSelectElement, MenutProps>(
-	({ currCalendars }, ref) => {
+	({ currCalendars, disabled = false }, ref) => {
 		let menuItems: ReserveMatrixData['menuItems'] = []
 		if (currCalendars) {
 			menuItems = [...currCalendars.menuItems]
 		}
 		// --- メニュー追加
 		return (
-			<select id="menu" ref={ref}>
+			<select id="menu" ref={ref} disabled={disabled}>
 				{menuItems.map((e, i) => (
 					<option key={`MenuOptions_${i}`} value={e.id}>
 						{e.name}({e.miniutes}分)
